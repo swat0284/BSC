3f411ca176ef699d90739a171998ae8e8ab5d999
pawel <swatinf@gmail.com>
2025-10-13 16:13:20 +0200

    other files

--- bsc/android/app/build.gradle.kts
+++ bsc/android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
+    
     namespace = "com.example.bsc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
